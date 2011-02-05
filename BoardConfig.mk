35479f7f35cb277aca319dd4c2f1d28aa0b55ee4
Tom Giordano <tomgiordano83@gmail.com>
2011-02-06 10:31:01 +1100

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,90 @@
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+LOCAL_PATH:= $(call my-dir)
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := false
+
+TARGET_NO_BOOTLOADER := true
+#TARGET_NO_RECOVERY := true
+#TARGET_NO_KERNEL := true
+#TARGET_NO_RADIOIMAGE := true
+
+TARGET_PREBUILT_RECOVERY_KERNEL := device/zte/blade/recovery_kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=blade console=ttyMSM2,115200
+
+TARGET_BOARD_PLATFORM := msm7k
+TARGET_ARCH_VARIANT := armv6-vfp
+TARGET_CPU_ABI := armeabi
+TARGET_CPU_ABI := armeabi-v6l
+TARGET_CPU_ABI2 := armeabi
+
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_BOOTLOADER_BOARD_NAME := blade
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_FM_RADIO := true
+
+TARGET_USES_2G_VM_SPLIT := true
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := AWEXT
+WIFI_DRIVER_MODULE_PATH     := "/system/wifi/ar6000.ko"
+WIFI_DRIVER_MODULE_NAME     := "ar6000"
+
+WITH_JIT := true
+ENABLE_JSC_JIT := true
+
+# OpenGL drivers config file path
+BOARD_EGL_CFG := device/zte/blade/egl.cfg
+
+# No fallback font by default (space savings)
+#NO_FALLBACK_FONT:=true
+
+BOARD_GPS_LIBRARIES := libloc_api
+
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+
+BOARD_KERNEL_BASE := 0x02a00000
+#BOARD_PAGE_SIZE := 0x00000800
+
+
+# # cat /proc/mtd
+# dev:    size   erasesize  name
+# mtd0: 00480000 00020000 "recovery"
+# mtd1: 00480000 00020000 "boot"
+# mtd2: 00180000 00020000 "splash"
+# mtd3: 00060000 00020000 "misc"
+# mtd4: 02940000 00020000 "cache"
+# mtd5: 0cf80000 00020000 "system"
+# mtd6: 0d020000 00020000 "userdata"
+# mtd7: 00180000 00020000 "persist"
+
+
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x00480000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x0cf80000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0d020000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/zte/blade/recovery/recovery_ui.c
