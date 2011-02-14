fb9e3e51c618a28667de6b89eb8880c0b35b051c
Tom Giordano <tomgiordano83@gmail.com>
2011-02-14 22:33:59 +1100

    Video problems were due to no libcopybit (not even the standard one). libcopybit provided by HCDRJacob (or maybe arcee).

--- libcopybit/Android.mk
+++ libcopybit/Android.mk
@@ -13,4 +13,20 @@
 # limitations under the License.
 
 
-include $(call all-named-subdir-makefiles, recovery libaudio libril libcamera libgralloc libcopybit)
+LOCAL_PATH:= $(call my-dir)
+# HAL module implemenation, not prelinked and stored in
+# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
+
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),blade)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SRC_FILES := copybit.cpp
+LOCAL_MODULE := copybit.blade
+LOCAL_C_INCLUDES += hardware/msm7k/libgralloc
+LOCAL_CFLAGS += -DCOPYBIT_MSM7K=1
+include $(BUILD_SHARED_LIBRARY)
+endif
+
